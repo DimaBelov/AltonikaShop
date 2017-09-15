e70fc8d0a381830b95bb984e697292918c3f817b
WILDBERRIES\Belov.Dmitriy3 <belov.dmitriy3@wildberries.ru>
2017-09-15 10:18:02 +0300

    controllers

---
+++ AltonikaShop.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using AltonikaShop.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AltonikaShop.WebApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ProductController : Controller
+    {
+        readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(_productService.GetAll());
+        }
+    }
+}
---
+++ AltonikaShop.WebApi/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AltonikaShop.Application.Services.Interfaces;
+using AltonikaShop.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AltonikaShop.WebApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class UserController : Controller
+    {
+        readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult Auth([FromBody] User user)
+        {
+            if(user == null)
+                throw new ArgumentNullException(nameof(user));
+
+            return Json(_userService.Auth(user));
+        }
+    }
+}
