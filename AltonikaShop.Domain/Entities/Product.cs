24af337ced2e8a474981db4209819fa5eea0a5c6
WILDBERRIES\Belov.Dmitriy3 <belov.dmitriy3@wildberries.ru>
2017-09-22 16:33:10 +0300

    price

--- AltonikaShop.Domain/Entities/Product.cs
+++ AltonikaShop.Domain/Entities/Product.cs
@@ -4,7 +4,7 @@ namespace AltonikaShop.Domain.Entities
 {
     public class Product : NamedEntity
     {
-        public string Code { get; set; }
+        public decimal? Price { get; set; }
 
         public string Description { get; set; }
         
