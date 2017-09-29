e93665e6b608a65a0e1103c77d569162aceabf88
WILDBERRIES\Belov.Dmitriy3 <belov.dmitriy3@wildberries.ru>
2017-09-29 18:54:07 +0300

    OrderDetail.Product Detach

--- AltonikaShop.Application/Services/OrderService.cs
+++ AltonikaShop.Application/Services/OrderService.cs
@@ -8,19 +8,34 @@ namespace AltonikaShop.Application.Services
 {
     public class OrderService : EntityService<Order>, IOrderService
     {
-        public OrderService(IEntityRepository<Order> repository) : base(repository)
+        readonly AppDbContext _appDbContext;
+
+        public OrderService(IEntityRepository<Order> repository, AppDbContext appDbContext) : base(repository)
         {
+            _appDbContext = appDbContext;
         }
 
         public IEnumerable<Order> GetByUser(int userId)
         {
-            return Repository.GetAllNoTrack(
+            return Repository.GetAll(
                 new Query<Order>
                 {
                     IsSatisfied = order => order.UserId == userId,
                     Include = orders => orders.Include(order => order.Details)
                         .ThenInclude(d => d.Product)
                 });
+        }
+
+        public override Order Add(Order order)
+        {
+            //order.Details.ForEach(d => d.Product = null);
+            //return Repository.Add(order).Entity;
+
+            var orderEntry = _appDbContext.Orders.Add(order);
+            orderEntry.Entity.Details.ForEach(d => 
+                _appDbContext.Entry(d.Product).State = EntityState.Detached);
+            _appDbContext.SaveChanges();
+            return orderEntry.Entity;
         }
     }
 }
