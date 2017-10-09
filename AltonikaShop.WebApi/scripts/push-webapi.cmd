029bee73c545c33e7209d205af804ecfbdd6a104
WILDBERRIES\Belov.Dmitriy3 <belov.dmitriy3@wildberries.ru>
2017-10-09 18:40:07 +0300

    bash script

--- AltonikaShop.WebApi/scripts/push-webapi.cmd
+++ AltonikaShop.WebApi/scripts/push-webapi.cmd
@@ -1,5 +1,9 @@
-set RU_SERVER_PW=2944939
-putty -ssh -pw %RU_SERVER_PW% u2944939@185.26.114.186 -m %GITHUB_PATH%/AltonikaShop/AltonikaShop.WebApi/scripts/webapi-clear-script -t
-psftp u2944939@185.26.114.186 -pw %RU_SERVER_PW% -b %GITHUB_PATH%\AltonikaShop\AltonikaShop.WebApi\scripts\webapi-put-script -be
-putty -ssh -pw %RU_SERVER_PW% u2944939@185.26.114.186 -m %GITHUB_PATH%/AltonikaShop/AltonikaShop.WebApi/scripts/webapi-run-script -t
-set RU_SERVER_PW=
+# putty -ssh u2944939@185.26.114.186 -m %GITHUB_PATH%/AltonikaShop/AltonikaShop.WebApi/scripts/webapi-clear-script -t
+# psftp u2944939@185.26.114.186 -b %GITHUB_PATH%\AltonikaShop\AltonikaShop.WebApi\scripts\webapi-put-script -be
+# putty -ssh u2944939@185.26.114.186 -m %GITHUB_PATH%/AltonikaShop/AltonikaShop.WebApi/scripts/webapi-run-script -t
+
+putty -ssh u2944939@185.26.114.186 -m scripts/webapi-clear-script -t
+psftp u2944939@185.26.114.186 -b scripts\webapi-put-script -be
+putty -ssh u2944939@185.26.114.186 -m scripts/webapi-run-script -t
+
+bash -c "ssh 185.26.114.186 -l u2944939 'echo Stop service and delete files && sh altonika-shop/scripts/webapi-clear.sh && exit' && echo Push new files && scp -r /mnt/c/GitHub/AltonikaShop/AltonikaShop.WebApi/bin/Release/PublishOutput/. u2944939@185.26.114.186:/home/u2944939/altonika-shop/webapi && ssh 185.26.114.186 -l u2944939 'echo Run service && sh altonika-shop/scripts/webapi-run.sh && exit'"
