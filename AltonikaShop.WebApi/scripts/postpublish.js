d8e5133f3a73a6c192866a4aca315701906d7841
WILDBERRIES\Belov.Dmitriy3 <belov.dmitriy3@wildberries.ru>
2017-10-09 20:52:55 +0300

    postpublish script

--- AltonikaShop.WebApi/scripts/postpublish.js
+++ AltonikaShop.WebApi/scripts/postpublish.js
@@ -1,10 +1,34 @@
 ﻿var spawn = require('child_process').spawn;
-var child =
-    spawn(
-        'putty -ssh u2944939@185.26.114.186 -m %GITHUB_PATH%/AltonikaShop/AltonikaShop.WebApi/scripts/webapi-clear-script -t\n' +
-        'psftp u2944939@185.26.114.186 -b %GITHUB_PATH%\AltonikaShop\AltonikaShop.WebApi\scripts\webapi-put-script -be\n' +
-        'putty -ssh u2944939@185.26.114.186 -m %GITHUB_PATH%/AltonikaShop/AltonikaShop.WebApi/scripts/webapi-run-script -t', {
-            windowsVerbatimArguments: true,
-            shell: true,
-            stdio: 'inherit'
-        });
+var fileExists = require('file-exists');
+
+var bashScript = 'bash -c "ssh 185.26.114.186 -l u2944939 \'echo Stop service and delete files && sh altonika-shop/scripts/webapi-clear.sh && exit\' && echo Push new files && scp -r /mnt/c/GitHub/AltonikaShop/AltonikaShop.WebApi/bin/Release/PublishOutput/. u2944939@185.26.114.186:/home/u2944939/altonika-shop/webapi && ssh 185.26.114.186 -l u2944939 \'echo Run service && sh altonika-shop/scripts/webapi-run.sh && exit\'"';
+var openSshScript = "ssh 185.26.114.186 -l u2944939 'echo Stop service and delete files && sh altonika-shop/scripts/webapi-clear.sh && exit' && echo Push new files && scp -r /mnt/c/GitHub/AltonikaShop/AltonikaShop.WebApi/bin/Release/PublishOutput/. u2944939@185.26.114.186:/home/u2944939/altonika-shop/webapi && ssh 185.26.114.186 -l u2944939 'echo Run service && sh altonika-shop/scripts/webapi-run.sh && exit'";
+
+fileExists('C:/Windows/System32/bash.exe').then(exists => {
+    if (exists) {
+        console.log('Run bash script');
+        runBashScript();
+    } else {
+        console.log('Run OpenSSh script');
+        runOpenSshScript();
+    }
+});
+
+runBashScript = function () {
+    var child =
+        spawn(
+            bashScript, {
+                shell: true,
+                stdio: 'inherit'
+            });
+}
+
+runOpenSshScript = function () {
+    var child =
+        spawn(
+            openSshScript, {
+                windowsVerbatimArguments: true,
+                shell: true,
+                stdio: 'inherit'
+            });
+}
--- AltonikaShop.WebApi/scripts/push-webapi.cmd
+++ AltonikaShop.WebApi/scripts/push-webapi.cmd
@@ -6,4 +6,6 @@ putty -ssh u2944939@185.26.114.186 -m scripts/webapi-clear-script -t
 psftp u2944939@185.26.114.186 -b scripts\webapi-put-script -be
 putty -ssh u2944939@185.26.114.186 -m scripts/webapi-run-script -t
 
+ssh 185.26.114.186 -l u2944939 'echo Stop service and delete files && sh altonika-shop/scripts/webapi-clear.sh && exit' && echo Push new files && scp -r /mnt/c/GitHub/AltonikaShop/AltonikaShop.WebApi/bin/Release/PublishOutput/. u2944939@185.26.114.186:/home/u2944939/altonika-shop/webapi && ssh 185.26.114.186 -l u2944939 'echo Run service && sh altonika-shop/scripts/webapi-run.sh && exit'
+
 bash -c "ssh 185.26.114.186 -l u2944939 'echo Stop service and delete files && sh altonika-shop/scripts/webapi-clear.sh && exit' && echo Push new files && scp -r /mnt/c/GitHub/AltonikaShop/AltonikaShop.WebApi/bin/Release/PublishOutput/. u2944939@185.26.114.186:/home/u2944939/altonika-shop/webapi && ssh 185.26.114.186 -l u2944939 'echo Run service && sh altonika-shop/scripts/webapi-run.sh && exit'"
